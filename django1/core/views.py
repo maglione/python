c2fde1711bb4887607ca97b7fbe1f5bf847e04d9
Daniel Vancetto Maglione <daniel@maglione.com.br>
2020-07-22 00:27:36 -0300

    Basico - Usando e abusando do Django Shell

--- django1/core/views.py
+++ django1/core/views.py
@@ -2,9 +2,15 @@ from django.shortcuts import render
 
 
 def index(request):
+    if str(request.user) == 'AnonymousUser':
+        logado = 'Usuário não logado'
+    else:
+        logado = f'Usuário logado: {str(request.user)}'
+
     context = {
         'curso': 'Programação Web com Django Framework',
-        'outro': 'Django é massa!'
+        'outro': 'Django é massa!',
+        'logado': logado
     }
     return render(request, 'index.html', context)
 
