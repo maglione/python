347839f43c32165a80105a6a31b89900b4aee51a
Daniel Vancetto Maglione <daniel@maglione.com.br>
2020-07-22 00:10:17 -0300

    Basico - A area administrativa dos projetos Django e o arquivo admin.py

--- django1/core/models.py
+++ django1/core/models.py
@@ -6,8 +6,15 @@ class Produto(models.Model):
     preco = models.DecimalField('Preço', decimal_places=2, max_digits=8)
     estoque = models.IntegerField('Quantidade em Estoque')
 
+    # Coloca o label na interface administrativa
+    def __str__(self):
+        return f'{self.nome}'
+
 
 class Cliente(models.Model):
     nome = models.CharField('Nome', max_length=100)
-    sobrenome = models.CharField('Nome', max_length=100)
-    email = models.EmailField('Nome', max_length=100)
+    sobrenome = models.CharField('Sobrenome', max_length=100)
+    email = models.EmailField('E-Mail', max_length=100)
+
+    def __str__(self):
+        return f'{self.nome} {self.sobrenome} - {self.email}'
