04730356418d05fe791d29702ca2b2b47169183f
Daniel Vancetto Maglione <daniel@maglione.com.br>
2020-07-22 23:12:54 -0300

    Basico - Apresentando dados do banco de dados no template

--- django1/core/views.py
+++ django1/core/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 
+from .models import Produto
+
 
 def index(request):
     if str(request.user) == 'AnonymousUser':
@@ -7,13 +9,23 @@ def index(request):
     else:
         logado = f'Usuário logado: {str(request.user)}'
 
+    produtos = Produto.objects.all()
+
     context = {
         'curso': 'Programação Web com Django Framework',
         'outro': 'Django é massa!',
-        'logado': logado
+        'logado': logado,
+        'produtos': produtos
     }
     return render(request, 'index.html', context)
 
 
 def contato(request):
     return render(request, 'contato.html')
+
+
+def produto(request, pk):
+    context = {
+        'produto': Produto.objects.get(id=pk)
+    }
+    return render(request, 'produto.html', context)
